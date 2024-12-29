f37c819b72c8bc87c27a1a2f2ba628cf01e7cc73
MeHacking <mihajlovic1337@gmail.com>
2024-12-29 16:16:05 +0100

    Filtriran sadrzaj login stranice
    U zavisnosti od toga da li je korisnik prijavljen ili nije prikazuju login-register/logout opcije

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -13,11 +13,21 @@ export class LoginPage implements OnInit {
   email: string = '';
   password: string = '';
   errorMessage: string = '';
+  isLoggedIn: boolean = false;
 
   constructor(private router: Router) {}
 
   ngOnInit() {
-   const app = initializeApp(environment.firebase);
+    // Inicijalizacija Firebase aplikacije
+    const app = initializeApp(environment.firebase);
+
+    // Proveriti da li je korisnik već ulogovan na osnovu 'userId' u localStorage
+    const userId = localStorage.getItem('userId');
+    if (userId && userId !== 'izlogovan') {
+      this.isLoggedIn = true;
+    } else {
+      this.isLoggedIn = false;
+    }
   }
 
   async login() {
@@ -26,6 +36,7 @@ export class LoginPage implements OnInit {
       const userCredential = await signInWithEmailAndPassword(auth, this.email, this.password);
       const userId = userCredential.user.uid;
       localStorage.setItem('userId', userId); // Smestanje u lokalnu memoriju
+      this.isLoggedIn = true;
       this.router.navigate(['/home']);  // Preusmeravanje na stranicu nakon prijave
     } catch (error: any) {
       this.errorMessage = error.message;
@@ -37,6 +48,7 @@ export class LoginPage implements OnInit {
     try {
       const userCredential = await createUserWithEmailAndPassword(auth, this.email, this.password);
       const userId = userCredential.user.uid;
+      localStorage.setItem('userId', userId); // Smestanje u lokalnu memoriju
       this.router.navigate(['/home']);  // Preusmeravanje na stranicu nakon registracije
     } catch (error: any) {
       this.errorMessage = error.message;
@@ -48,7 +60,8 @@ export class LoginPage implements OnInit {
     try {
       await signOut(auth);
       localStorage.setItem('userId', "izlogovan"); // Brisanje iz lokalne memorije
-      this.router.navigate(['/home']);  // Preusmeravanje na login stranicu nakon odjave
+      this.isLoggedIn = false;
+      this.router.navigate(['/login']);  // Preusmeravanje na login stranicu nakon odjave
     } catch (error: any) {
       this.errorMessage = error.message;
     }
